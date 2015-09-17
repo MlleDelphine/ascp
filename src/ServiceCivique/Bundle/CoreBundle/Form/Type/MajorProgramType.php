a728ce493f7b131d85b1d2ff8f8ca178bb43460b
frederick@zephyr-web.fr <frederick@zephyr-web.fr>
2015-09-17 17:49:56 +0200

    - Ticket "Évolutions #25" : traitement dynamique du sélecteur de position.

--- src/ServiceCivique/Bundle/CoreBundle/Form/Type/MajorProgramType.php
+++ src/ServiceCivique/Bundle/CoreBundle/Form/Type/MajorProgramType.php
@@ -2,12 +2,20 @@
 
 namespace ServiceCivique\Bundle\CoreBundle\Form\Type;
 
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class MajorProgramType extends AbstractType
 {
+    public function __construct(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -16,14 +24,35 @@ class MajorProgramType extends AbstractType
         $builder
             ->add('title', 'text', array('label' => 'Titre'))
             ->add('url', 'text', array('label' => 'URL'))
-            ->add('position', 'text', array('label' => 'Position'))
             ->add('file', 'file', array('label' => 'Fichier', 'required' => false));
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $data = $event->getData();
+
+                $count = count($this->repository->findAll());
+                $count = ($count > 1 ? $count : 1);
+                if (!$data->getId()) {
+                    $count += 1;
+                }
+                $choices = range(1, $count);
+
+                $form->add('position', 'choice', array(
+                        'label' => 'Position',
+                        'choices' => $choices,
+                    )
+                );
+
+            }
+        );
 
         $builder->add('actions', 'form_actions');
 
         $builder->add('save', 'submit', array(
             'label' => 'Valider',
-            'attr' => array('class' => 'btn btn-sc-red')
+            'attr' => array('class' => 'btn btn-sc-red'),
         ));
     }
 
@@ -33,8 +62,8 @@ class MajorProgramType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class'         => 'ServiceCivique\Bundle\CoreBundle\Entity\MajorProgram',
-            'cascade_validation' => true
+            'data_class' => 'ServiceCivique\Bundle\CoreBundle\Entity\MajorProgram',
+            'cascade_validation' => true,
         ));
     }
 
