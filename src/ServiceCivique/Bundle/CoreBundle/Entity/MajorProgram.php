a97ec855f4c61b037b7cf0a3731d4890f89fd4ae
frederick@zephyr-web.fr <frederick@zephyr-web.fr>
2015-09-29 16:46:18 +0200

    - Tcket #25 : Refactoring de l'entité, suppression du champ $path et des lifecycle callback autour du transfert du fichier.

--- src/ServiceCivique/Bundle/CoreBundle/Entity/MajorProgram.php
+++ src/ServiceCivique/Bundle/CoreBundle/Entity/MajorProgram.php
@@ -58,11 +58,6 @@ class MajorProgram implements RoutedItemInterface
     /**
      * @var string
      */
-    private $path;
-
-    /**
-     * @var string
-     */
     private $url;
 
     /**
@@ -80,8 +75,6 @@ class MajorProgram implements RoutedItemInterface
      */
     private $published;
 
-    private $file;
-    private $temp;
 
     /**
      * Get id.
@@ -190,30 +183,6 @@ class MajorProgram implements RoutedItemInterface
     }
 
     /**
-     * Set path.
-     *
-     * @param string $path
-     *
-     * @return MajorProgram
-     */
-    public function setPath($path)
-    {
-        $this->path = $path;
-
-        return $this;
-    }
-
-    /**
-     * Get path.
-     *
-     * @return string
-     */
-    public function getPath()
-    {
-        return $this->path;
-    }
-
-    /**
      * Set slug.
      *
      * @param string $slug
@@ -235,110 +204,6 @@ class MajorProgram implements RoutedItemInterface
     public function getSlug()
     {
         return $this->slug;
-    }
-
-    /**
-     * Get file.
-     *
-     * @return UploadedFile
-     */
-    public function getFile()
-    {
-        return $this->file;
-    }
-
-    public function getAbsolutePath()
-    {
-        return null === $this->path
-            ? null
-            : $this->getUploadRootDir().'/'.$this->path;
-    }
-
-    public function getWebPath()
-    {
-        return null === $this->path
-            ? null
-            : $this->getUploadDir().'/'.$this->path;
-    }
-
-    protected function getUploadRootDir()
-    {
-        // the absolute directory path where uploaded
-        // documents should be saved
-        return __DIR__.'/../../../../../web/'.$this->getUploadDir();
-    }
-
-    protected function getUploadDir()
-    {
-        // get rid of the __DIR__ so it doesn't screw up
-        // when displaying uploaded doc/image in the view.
-        return 'uploads/major_programs';
-    }
-
-    /**
-     * Sets file.
-     *
-     * @param UploadedFile $file
-     */
-    public function setFile(UploadedFile $file = null)
-    {
-        $this->file = $file;
-        // check if we have an old image path
-        if (isset($this->path)) {
-            // store the old name to delete after the update
-            $this->temp = $this->path;
-            $this->path = null;
-        } else {
-            $this->path = 'initial';
-        }
-    }
-
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     */
-    public function preUpload()
-    {
-        if (null !== $this->getFile()) {
-            // do whatever you want to generate a unique name
-            $filename   = sha1(uniqid(mt_rand(), true));
-            $this->path = $filename.'.'.$this->getFile()->guessExtension();
-        }
-    }
-
-    /**
-     * @ORM\PostPersist()
-     * @ORM\PostUpdate()
-     */
-    public function upload()
-    {
-        if (null === $this->getFile()) {
-            return;
-        }
-
-        // if there is an error when moving the file, an exception will
-        // be automatically thrown by move(). This will properly prevent
-        // the entity from being persisted to the database on error
-        $this->getFile()->move($this->getUploadRootDir(), $this->path);
-
-        // check if we have an old image
-        if (isset($this->temp)) {
-            // delete the old image
-            unlink($this->getUploadRootDir().'/'.$this->temp);
-            // clear the temp image path
-            $this->temp = null;
-        }
-        $this->file = null;
-    }
-
-    /**
-     * @ORM\PostRemove()
-     */
-    public function removeUpload()
-    {
-        if ($file = $this->getAbsolutePath()) {
-            unlink($file);
-        }
     }
 
     /**
